5a2b4fc5aa64a5740a8061d00f8d775d8902c15d
Admin <admin@test.com>
2026-05-04 20:16:17 +0700

    fix: revert wildcard route to * to prevent startup crash

--- bff/src/proxy/proxy/proxy.controller.ts
+++ bff/src/proxy/proxy/proxy.controller.ts
@@ -14,7 +14,7 @@ export class ProxyController {
     private readonly authService: AuthService,
   ) {}
 
-  @All(':path*')
+  @All('*')
   async proxy(@Req() req: Request, @Res() res: Response) {
     const path = req.path;
     
